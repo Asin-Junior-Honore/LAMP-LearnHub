08b47972dcd8c59ee21ec559b875004e2cb0db75
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-06-15 03:17:41 +0100

    added header section

--- includes/header.php
+++ includes/header.php
@@ -1,19 +1,59 @@
 <!-- header.php -->
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LearnHub - Online Learning Platform</title>
+    <title>Navigation Bar</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body>
-    <nav>
-        <a href="index.php">Home</a> |
-        <a href="courses/create_course.php">Create Course</a> |
-        <a href="users/login.php">Login</a> |
-        <a href="users/register.php">Register</a> |
-        <a href="users/logout.php">Logout</a>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <h2>LearnHub</h2>
+            <!-- <img src="path/to/logo.png" width="30" height="30" alt="Logo"> -->
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Courses</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <div class="ml-auto">
+            <ul class="navbar-nav  align-items-center">
+                <li class="nav-item login">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item signupbtn">
+                    <a class="nav-link" href="#">Sign Up</a>
+                </li>
+            </ul>
+        </div>
     </nav>
-    <div class="container">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
