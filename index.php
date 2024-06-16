c0c022c326d40f970c881b5b8a406add370e870a
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-06-16 14:58:41 +0100

    added section 4

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!-- index.php -->
 
 <?php include 'includes/header.php'; ?>
-<section class="container-xl py-5">
-    <div class="row align-items-center">
-        <aside class="col-md-6 text-center text-md-left mb-4 mb-md-0">
+<section class=" hero-section text-center d-flex align-items-center justify-content-center">
+    <div class="row container-xl pt-5 align-items-center ">
+        <aside class="col-md-6 text-center text-md-left mb-4 mb-md-0 p-0 ">
             <h1 class="display-4">The Best Online Course You'll Find</h1>
             <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
                 ullamcorper mattis, pulvinar dapibus leo.</p>
@@ -30,10 +30,17 @@
         </aside>
         <aside class="col-md-6 text-center hero-image-container"></aside>
     </div>
-    <div class="row align-items-center mt-5">
-        <aside class="col-md-6 text-center hero-image-container2"></aside>
-        <aside class="col-md-6 text-center text-md-left">
-            <h2>Providing Amazing Online Courses</h2>
+</section>
+
+<main class="container-xl">
+    <div class="row align-items-center mt-5 section-2">
+        <aside class="col-md-6 text-center leftimg">
+            <img class="img-fluid"
+                src="https://askproject.net/studdy/wp-content/uploads/sites/43/2021/12/studio-shot-of-cute-thankful-clever-student-feels-RP65PDL-1.png"
+                alt="">
+        </aside>
+        <aside class="col-md-6 text-center text-md-left px-5">
+            <h2 class="display-4 "> Providing Amazing Online Courses</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
                 pulvinar dapibus leo.</p>
             <div>
@@ -48,6 +55,99 @@
             </div>
         </aside>
     </div>
-</section>
+
+    <section class="container py-5 px-0 mt-5 section-3">
+        <div class="mr-5">
+            <aside class="">
+                <h2 class="display-5 ">Premium Learning Experience</h2>
+                <p class="lead my-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed tincidunt velit.
+                    Donec bibendum turpis vitae maximus bibendum.</p>
+                <div class=" image-div">
+                    <h3 class="h4">Quality online courses for all!</h3>
+                    <ul class="list-inline mt-3">
+                        <li class="list-inline-item">
+                            <a class="btn btn-primary btn-lg" href="#">Get Started</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </aside>
+
+        </div>
+        <div class="p-0">
+            <div class="">
+                <div class="d-flex align-items-start">
+                    <span class="badge badge-primary mr-3">1</span>
+                    <div>
+                        <h3 class="h3">Online courses from experts</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sem ex, facilisis eget
+                            libero sed, mattis congue mauris.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="my-2">
+                <div class="d-flex align-items-start">
+                    <span class="badge badge-primary mr-3">2</span>
+                    <div>
+                        <h3 class="h3">Learn at your own pace</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sem ex, facilisis eget
+                            libero sed, mattis congue mauris.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="">
+                <div class="d-flex align-items-start">
+                    <span class="badge badge-primary mr-3">3</span>
+                    <div>
+                        <h3 class="h3">Achieve your goals</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sem ex, facilisis eget
+                            libero sed, mattis congue mauris.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-4 py-5">
+        <div class="text-center mb-5">
+            <h2>Browse By Category</h2>
+            <p>Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel <br />
+                tincidunt magna laoreet eu.</p>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 mb-4">
+                    <div class="category-box text-center p-4">
+                        <span class="mb-3">icon</span>
+                        <h3>Writing</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-4">
+                    <div class="category-box text-center p-4">
+                        <span class=" mb-3">icon</span>
+                        <h3>Writing</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-4">
+                    <div class="category-box text-center p-4">
+                        <span class="mb-3">icon</span>
+                        <h3>Writing</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-4">
+                    <div class="category-box text-center p-4">
+                        <span class=" mb-3">icon</span>
+                        <h3>Writing</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
 
 <?php include 'includes/footer.php'; ?>
