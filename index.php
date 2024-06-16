5f56eab462a6d514ddf73e1f62923b49fd0b7346
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-06-16 01:54:03 +0100

    added second section

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <aside class="col-md-6 text-center text-md-left mb-4 mb-md-0">
             <h1 class="display-4">The Best Online Course You'll Find</h1>
             <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
-                ullamcorper mattis, pulvinar dapibus.</p>
+                ullamcorper mattis, pulvinar dapibus leo.</p>
             <div>
                 <ul class="list-inline">
                     <li class="list-inline-item">
@@ -17,8 +17,7 @@
                     </li>
                 </ul>
             </div>
-
-            <div class="stats d-flex justify-content-between mt-4 ">
+            <div class="stats d-flex justify-content-between mt-4">
                 <div class="text-center">
                     <h3>940+</h3>
                     <p>Online users</p>
@@ -29,8 +28,24 @@
                 </div>
             </div>
         </aside>
-        <aside class="col-md-6 text-center hero-image-container">
-
+        <aside class="col-md-6 text-center hero-image-container"></aside>
+    </div>
+    <div class="row align-items-center mt-5">
+        <aside class="col-md-6 text-center hero-image-container2"></aside>
+        <aside class="col-md-6 text-center text-md-left">
+            <h2>Providing Amazing Online Courses</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+                pulvinar dapibus leo.</p>
+            <div>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a class="btn btn-primary btn-lg" href="#">▶️ Watch Video</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="btn btn-secondary btn-lg" href="#">📞 Contact us</a>
+                    </li>
+                </ul>
+            </div>
         </aside>
     </div>
 </section>
