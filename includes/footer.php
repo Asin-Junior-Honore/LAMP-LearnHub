8f7e43b6446089503794a39e36bc127b4b197640
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-06-25 08:47:37 +0100

    added footer section

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,35 @@
-<!-- footer.php -->
-
-</div><!-- .container -->
+</div>
 <footer>
-    &copy; <?php echo date('Y'); ?> LearnHub. All rights reserved.
+    <div class="container">
+        <hr>
+        <div class="row">
+            <div class="col-md-4">
+                <h5>About LearnHub</h5>
+                <p>LearnHub is a platform dedicated to providing quality education and resources for learners around the
+                    world.</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Courses</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5>Follow Us</h5>
+                <div class="social-icons">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <p>&copy; <?php echo date('Y'); ?> LearnHub. All rights reserved.</p>
+    </div>
 </footer>
 </body>
 
--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home |🧑‍💻</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/style.css">
 </head>
 
